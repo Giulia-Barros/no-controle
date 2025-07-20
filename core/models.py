1ee9040d048cfbf021cc995899ebd45265af868a
Giulia-Barros <130318382+Giulia-Barros@users.noreply.github.com>
2025-07-20 20:51:29 -0300

    create models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User #Usuário autenticados no sistema
 
-# Create your models here.
+class Tarefa(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Relação entre a tarefa e usuário, ou seja, cada tarefa pertence a um usuário.
+    descricao = models.CharField(max_length=255)
+    concluida = models.BooleanField(default=False)
+    criada = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.descricao
